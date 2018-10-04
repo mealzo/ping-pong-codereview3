463f2fc18ffcd48e69244da2ee0b48542c322f9e
Amelia Ralston-Okabayashi <a.ralston.o@gmail.com>
2018-10-03 23:50:56 -0700

    add the for loop and branching statements to get the correct results

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,40 @@
+// var pingPong = function(countTo){
+//   for (var i = 1; i < countTo; i++) {
+//     if (i%3===0) {
+//       $(".numbers").text("ping");
+//     }
+//     else if (i%5===0) {
+//       $(".numbers").text("pong");
+//     }
+//     else if (i%15===0) {
+//       $(".numbers").text("ping pong!");
+//     }
+//     else {
+//       $(".numbers").text(i);
+//     }
+//   }
+// }
+//
+
 $(document).ready(function(){
-  $("#submit").submit(function(event){
+  $("#form").submit(function(event){
+    var countTo = $("input#count-to").val();
+
+      for (var i = 1; i <= countTo; i++) {
+        if (i%3===0 && i%15!=0) {
+          $(".numbers").append("<li>ping</li>");
+        }
+        else if (i%5===0 && i%15!=0) {
+          $(".numbers").append("<li>pong</li>");
+        }
+        else if (i%15===0) {
+          $(".numbers").append("<li>ping pong!</li>");
+        }
+        else {
+          $(".numbers").append("<li>" + i + "</li>");
+        }
+      }
+
     event.preventDefault();
-    
   });
 });
