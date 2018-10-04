d216581615ec890aa7e0d02faeaa547d446d7b62
Amelia Ralston-Okabayashi <a.ralston.o@gmail.com>
2018-10-04 00:04:54 -0700

    spruce up logic issues and add the .empty method

--- js/scripts.js
+++ js/scripts.js
@@ -18,17 +18,18 @@
 
 $(document).ready(function(){
   $("#form").submit(function(event){
+    $(".numbers").empty();
     var countTo = $("input#count-to").val();
 
       for (var i = 1; i <= countTo; i++) {
         if (i%3===0 && i%15!=0) {
-          $(".numbers").append("<li>ping</li>");
+          $(".numbers").append("<li>Ping</li>");
         }
         else if (i%5===0 && i%15!=0) {
-          $(".numbers").append("<li>pong</li>");
+          $(".numbers").append("<li>Pong</li>");
         }
         else if (i%15===0) {
-          $(".numbers").append("<li>ping pong!</li>");
+          $(".numbers").append("<li>Ping Pong!</li>");
         }
         else {
           $(".numbers").append("<li>" + i + "</li>");
