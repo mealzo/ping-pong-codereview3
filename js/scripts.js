28d24a36357199aa417ac25e24fdc2ce889920d8
Amelia Ralston-Okabayashi <a.ralston.o@gmail.com>
2018-10-15 00:26:51 -0700

    change the code to seperate the business logic from user logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,40 +1,29 @@
-// var pingPong = function(countTo){
-//   for (var i = 1; i < countTo; i++) {
-//     if (i%3===0) {
-//       $(".numbers").text("ping");
-//     }
-//     else if (i%5===0) {
-//       $(".numbers").text("pong");
-//     }
-//     else if (i%15===0) {
-//       $(".numbers").text("ping pong!");
-//     }
-//     else {
-//       $(".numbers").text(i);
-//     }
-//   }
-// }
-//
+
+
+var pingPong = function(countTo){
+for (var i = 1; i <= countTo; i++) {
+  if (i%3===0 && i%15!=0) {
+    $(".numbers").append("<li>Ping</li>");
+  }
+  else if (i%5===0 && i%15!=0) {
+    $(".numbers").append("<li>Pong</li>");
+  }
+  else if (i%15===0) {
+    $(".numbers").append("<li>Ping Pong!</li>");
+  }
+  else {
+    $(".numbers").append("<li>" + i + "</li>");
+  }
+}
+}
 
 $(document).ready(function(){
   $("#form").submit(function(event){
     $(".numbers").empty();
     var countTo = $("input#count-to").val();
+    pingPong(countTo);
+    
 
-      for (var i = 1; i <= countTo; i++) {
-        if (i%3===0 && i%15!=0) {
-          $(".numbers").append("<li>Ping</li>");
-        }
-        else if (i%5===0 && i%15!=0) {
-          $(".numbers").append("<li>Pong</li>");
-        }
-        else if (i%15===0) {
-          $(".numbers").append("<li>Ping Pong!</li>");
-        }
-        else {
-          $(".numbers").append("<li>" + i + "</li>");
-        }
-      }
 
     event.preventDefault();
   });
